bb6f02af8a788c09037f45279105ddee266e1be1
leehao <1192263018@qq.com>
2020-12-08 15:42:04 +0800

    user=李浩,完成数据源列表分页

--- src/api/home/database.js
+++ src/api/home/database.js
@@ -1,8 +1,8 @@
-import {get} from "@/plugins/utils/request";
+import {get, post} from "@/plugins/utils/request";
 
 /* 数据源*/
 export let getTypes = param => get('dataSource/types');  //获取支持数据库类型
 
-export let getDatabaseList = param => get('/datamiddleend/dataSource/findList');  //获取数据源列表
+export let getDatabaseList = param => post('/datamiddleend/dataSource/findList', param);  //获取数据源列表
 
 
--- src/views/database/DatabaseList.vue
+++ src/views/database/DatabaseList.vue
@@ -1,8 +1,22 @@
 <template>
-  <a-table :pagination="false" :columns="columns" :data-source="databaseList">
-    <a slot="name" slot-scope="text">{{ text }}</a>
-  </a-table>
+  <div id="app">
+    <a-table rowKey="id" :pagination="false" :columns="columns" :data-source="databaseList">
+      <a slot="name" slot-scope="text">{{ text }}</a>
+    </a-table>
+
+    <div class="divPagin">
+      <a-pagination v-model:current="pageIndex" :page-size-options="pageSizeOptions" :total="count" show-size-changer
+                    :page-size="pageSize" @showSizeChange="onShowSizeChange" @change="getList">
+        <template #buildOptionText="props">
+          <span v-if="props.value !== '50'">{{ props.value }}条/页</span>
+          <span v-else>全部</span>
+        </template>
+      </a-pagination>
+    </div>
+  </div>
+
 </template>
+
 <script>
 
 import {getDatabaseList} from "@/api/home/database";
@@ -22,8 +36,8 @@ const columns = [
   },
   {
     title: '主机地址',
-    dataIndex: 'address',
-    key: 'address',
+    dataIndex: 'hostName',
+    key: 'hostName',
     ellipsis: true,
   },
   {
@@ -40,40 +54,19 @@ const columns = [
     ellipsis: true,
   },
   {
-    title: 'Action',
+    title: '操作',
     key: 'action',
     scopedSlots: {customRender: 'action'},
-  },
-];
-
-const data = [
-  {
-    id: '1',
-    databaseName: 'fromdb',
-    address: '192.168.0.1',
-    port: '3306',
-    databaseType: 'MySql',
-  },
-  {
-    id: '2',
-    databaseName: 'todb',
-    address: '192.168.0.1',
-    port: '3306',
-    databaseType: 'MySql',
-  },
-  {
-    id: '3',
-    databaseName: 'test',
-    address: '192.168.0.1',
-    port: '3306',
-    databaseType: 'MySql',
   },
 ];
 
 export default {
   data() {
     return {
-      data,
+      pageIndex: 1,
+      pageSize: 10,
+      pageSizeOptions: ['1', '3', '5', '10', '50'],
+      count: 0,
       columns,
       databaseList: [],
     };
@@ -81,9 +74,17 @@ export default {
     this.getList()
   }, methods: {
     async getList() {
-      let r = await getDatabaseList();
-      this.databaseList = r.data
+      let r = await getDatabaseList({
+        "pageIndex": this.pageIndex,
+        "pageSize": this.pageSize
+      });
+      this.databaseList = r.data.list
+      this.count = r.data.total
+    },
+    onShowSizeChange(pageIndex, pageSize) {
+      this.pageSize = pageSize;
+      this.getList()
     }
-  }
+  },
 };
 </script>
