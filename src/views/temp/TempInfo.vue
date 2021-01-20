ddf657faccc58b7a53c92633b26965a8d380311c
leehao <1192263018@qq.com>
2021-01-20 16:07:31 +0800

    user=李浩，模板详情页面提交

--- src/views/temp/TempInfo.vue
+++ src/views/temp/TempInfo.vue
@@ -2,20 +2,23 @@
   <div style="background-color: #ececec; padding: 20px;">
     <a-row :gutter="16">
       <a-col :span="10">
-        <a-card title="身份证" :bordered="true">
+        <a-card title="模板缩略图" :bordered="true">
           <a-card-grid style="width:100%;text-align:center">
             <img src="../../assets/sfz.jpg"/>
+            <a-card-meta title="身份证" style="margin-top: 10px">
+            </a-card-meta>
           </a-card-grid>
+
         </a-card>
       </a-col>
-      <a-col :span="13">
+      <a-col :span="14">
         <a-card title="模板扩展数据信息" :bordered="true">
           <a-table ref="table" rowKey="id" :pagination="false" :columns="columns" :data-source="kzList" :alert="true"
                    showPagination="auto">
           </a-table>
-          <div class="divPagin">
+          <div class="divPagin" style="margin-top: 10px">
             <a-pagination v-model:current="pageIndex" :page-size-options="pageSizeOptions" :total="count" show-size-changer
-                          :page-size="pageSize" @showSizeChange="onShowSizeChange" @change="kzList">
+                          :page-size="pageSize" @showSizeChange="onShowSizeChange" @change="getList">
               <template #buildOptionText="props">
                 <span v-if="props.value !== '50'">{{ props.value }}条/页</span>
                 <span v-else>全部</span>
@@ -23,7 +26,9 @@
             </a-pagination>
           </div>
         </a-card>
+
       </a-col>
+
     </a-row>
   </div>
 </template>
@@ -73,14 +78,6 @@ export default {
         {id: 2, kzKey: "KZ_CSRQ", kzName: "出生日期", type: "varchar",length:12},
         {id: 2, kzKey: "KZ_CSRQ", kzName: "出生日期", type: "varchar",length:12},
         {id: 2, kzKey: "KZ_CSRQ", kzName: "出生日期", type: "varchar",length:12},
-        {id: 2, kzKey: "KZ_CSRQ", kzName: "出生日期", type: "varchar",length:12},
-        {id: 2, kzKey: "KZ_CSRQ", kzName: "出生日期", type: "varchar",length:12},
-        {id: 2, kzKey: "KZ_CSRQ", kzName: "出生日期", type: "varchar",length:12},
-        {id: 2, kzKey: "KZ_CSRQ", kzName: "出生日期", type: "varchar",length:12},
-        {id: 2, kzKey: "KZ_CSRQ", kzName: "出生日期", type: "varchar",length:12},
-        {id: 2, kzKey: "KZ_CSRQ", kzName: "出生日期", type: "varchar",length:12},
-        {id: 2, kzKey: "KZ_CSRQ", kzName: "出生日期", type: "varchar",length:12},
-
       ]
     };
   },
